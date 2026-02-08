2d997908104c76947c95d23cd2cf4ecade83386b
davinziii <lester69vin@gmail.com>
2026-02-08 22:51:39 +0800

    final update

--- src/layout/DesignProject.jsx
+++ src/layout/DesignProject.jsx
@@ -12,6 +12,7 @@ function DesignProject() {
     const [activeGallery, setActiveGallery] = useState('profile')
     const [isVisible, setIsVisible] = useState(true)
 
+    // Other bento design handle
     const handleGallery = (galleryName) => {
 
         if (galleryName === activeGallery) return
@@ -21,9 +22,10 @@ function DesignProject() {
         setTimeout(() => {
             setActiveGallery(galleryName)
             setIsVisible(true) 
-        }, 300) 
+        }, 1000) 
     }
 
+    // Other bento design
     const galleryData = {
         profile: (
             <div className='gallery-content grid1'>
@@ -63,18 +65,50 @@ function DesignProject() {
         )
     }
 
+
+
     return(
         <>
             <div className='design-project'>
                 <h1 className='centered-title hidden hidden-up'>Drawing and Animation Projects</h1>
-                <div className='card hidden hidden-up'>
+                {/* <div className='card hidden hidden-up'>
                     <img id='profile' src={profile} onClick={() => handleGallery("profile")} className={activeGallery === "profile" ? "active" : "inactive"} alt="" draggable={false} />
                     <img id='okd' src={okd} onClick={() => handleGallery("okd")} className={activeGallery === "okd" ? "active" : "inactive"} alt="" draggable={false} />
                     <img id='marms' src={marms} onClick={() => handleGallery("marms")} className={activeGallery === "marms" ? "active" : "inactive"} alt="" draggable={false} />
-                </div>
-                <div className='gallery-container hidden hidden-up'>
+                </div> */}
+                <div className='gallery-container'>
                     <div className={`gallery ${isVisible ? "show" : "hide"}`}>
-                        {galleryData[activeGallery]}
+                        {/* {galleryData[activeGallery]} */}
+                        <div className='gallery-content grid1'>
+                            <div className='box profile1 hidden hidden-up'><img src={profileGallery[0].icon} alt=""/></div>
+                            <div className='box profile2 hidden hidden-up'><img src={profileGallery[5].icon} alt=""/></div>
+                            <div className='box profile3 hidden hidden-up'><img src={profileGallery[3].icon} alt=""/></div>
+                            <div className='box profile4 hidden hidden-up'><img src={profileGallery[6].icon} alt=""/></div>
+                            <div className='box profile5 hidden hidden-up'><img src={profileGallery[4].icon} alt=""/></div>
+                            <div className='box profile6 hidden hidden-up'><img src={profileGallery[1].icon} alt=""/></div>
+                            <div className='box profile7 hidden hidden-up'><img src={profileGallery[2].icon} alt=""/></div>
+                            <div className='box profile8 hidden hidden-up'><img src={profileGallery[7].icon} alt=""/></div>
+                            <div className='box profile9 hidden hidden-up'><img src={profileGallery[8].icon} alt=""/></div>
+                        </div>
+                        <div className='gallery-content grid2'>
+                            <div className='box okd1 hidden hidden-up'><img src={okdGallery[0].icon} alt=""/></div>
+                            <div className='box okd2 hidden hidden-up'><img src={okdGallery[7].icon} alt=""/></div>
+                            <div className='box okd3 hidden hidden-up'><img src={okdGallery[2].icon} alt=""/></div>
+                            <div className='box okd4 hidden hidden-up'><img src={okdGallery[3].icon} alt=""/></div>
+                            <div className='box okd5 hidden hidden-up'><img src={okdGallery[4].icon} alt=""/></div>
+                            <div className='box okd6 hidden hidden-up'><img src={okdGallery[5].icon} alt=""/></div>
+                            <div className='box okd7 hidden hidden-up'><img src={okdGallery[6].icon} alt=""/></div>
+                            <div className='box okd8 hidden hidden-up'><img src={okdGallery[1].icon} alt=""/></div>
+                            <div className='box okd9 hidden hidden-up'><img src={okdGallery[8].icon} alt=""/></div>
+                        </div>
+                        <div className='gallery-content grid3'>
+                            <div className='box marms1 hidden hidden-up'><img src={marmsGallery[0].icon} alt=""/></div>
+                            <div className='box marms2 hidden hidden-up'><img src={marmsGallery[5].icon} alt=""/></div>
+                            <div className='box marms3 hidden hidden-up'><img src={marmsGallery[2].icon} alt=""/></div>
+                            <div className='box marms4 hidden hidden-up'><img src={marmsGallery[3].icon} alt=""/></div>
+                            <div className='box marms5 hidden hidden-up'><img src={marmsGallery[4].icon} alt=""/></div>
+                            <div className='box marms6 hidden hidden-up'><img src={marmsGallery[1].icon} alt=""/></div>
+                        </div>
                     </div>
                 </div>
             </div>
